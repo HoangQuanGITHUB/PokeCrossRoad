86e80b7ff6aa07852945839bbde05bb8ad26690c
[HoangQuanGITHUB] <mcinotgoodatgame@gmail.com>
2023-10-15 17:00:58 +0700

    Mess around

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ app = Ursina(development_mode=False)
 filter=CommonFilters(app.win,app.cam)
 
 Audio('theme.mp3',loop=True)
-
 poke=Entity(model='poke',shader=lit_with_shadows_shader,y=1.2)
 for z in range(10):
     Entity(model='road',shader=lit_with_shadows_shader,z=z*25)
