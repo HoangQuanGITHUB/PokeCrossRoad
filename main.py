e07cd6f812e530b88defe68dd6897ade89bdcf2e
HoangQuanGITHUB <mcinotgoodatgame@gmail.com>
2023-10-15 13:58:14 +0700

    Added some effects

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ poke=Entity(model='poke',shader=lit_with_shadows_shader,y=1.2)
 for z in range(10):
     Entity(model='road',shader=lit_with_shadows_shader,z=z*25)
 Sky(texture='sky_sunset')
-filter.setMSAA(16)
-filter.setBloom(intensity=.2)
+
+filter.setCartoonInk()
+filter.setMSAA(32)
+filter.setBloom(intensity=.5)
+
 camera_pivot=Entity()
 camera.parent=camera_pivot
 camera.position=(0,.5,-30)
