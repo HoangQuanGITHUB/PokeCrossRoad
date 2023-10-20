805125f56a73a11cade1b796c38e84f4173303a3
[HoangQuanGITHUB] <mcinotgoodatgame@gmail.com>
2023-10-20 22:32:44 +0700

    Fixed few bugs

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Audio('theme.mp3',loop=True)
 shader=lit_with_shadows_shader
 filter=CommonFilters(app.win,app.cam)
 poke=Entity(model='poke',shader=shader,y=1.2,z=-10)
+poker=Entity(parent=poke)
 start_point=poke.position
 road=Entity(shader=shader)
 for z in range(3):
@@ -24,17 +25,28 @@ camera.parent=camera_pivot
 camera.position=(0,1,-30)
 camera_pivot.rotation_y=-30 
 camera_pivot.rotation_x=20
+moving=False
 
 def update():
-    if held_keys['w']:
-        poke.rotation_y=lerp(poke.rotation_y,0,time.dt*10)
-    elif held_keys['s']:
-        poke.rotation_y=lerp(poke.rotation_y,180,time.dt*10)
-    elif held_keys['a']:
-        poke.rotation_y=lerp(poke.rotation_y,-90,time.dt*10)
-    elif held_keys['d']:
-        poke.rotation_y=lerp(poke.rotation_y,90,time.dt*10)
-    if (held_keys['w'] or held_keys['s'] or held_keys['a'] or held_keys['d']) and (poke.x > -15.3379 and poke.x < 22.9091):
+    global moving
+    moving=False
+    if held_keys['w'] and not moving:
+        poker.rotation_y=0
+        poke.rotation_y=slerp(poke.quaternion,poker.quaternion,time.dt*10)
+        moving=True
+    if held_keys['s'] and not moving:
+        poker.rotation_y=180
+        poke.rotation_y=slerp(poke.quaternion,poker.quaternion,time.dt*10)
+        moving=True
+    if held_keys['a'] and not moving:
+        poker.rotation_y=-90
+        poke.rotation_y=slerp(poke.quaternion,poker.quaternion,time.dt*10)
+        moving=True
+    if held_keys['d'] and not moving:
+        poker.rotation_y=90
+        poke.rotation_y=slerp(poke.quaternion,poker.quaternion,time.dt*10)
+        moving=True
+    if moving and (poke.x > -15.3379 and poke.x < 22.9091):
         poke.position+=poke.forward*time.dt*5
     elif poke.x<=-15.3379:
         poke.x+=time.dt
@@ -57,5 +69,6 @@ def input(key):
 
 pivot = Entity()
 light=DirectionalLight(parent=pivot, y=2, z=3, shadows=True, rotation=(45, 90, 45))
+light.shadow_map_resolution = (1024,1024)
 
 app.run()
