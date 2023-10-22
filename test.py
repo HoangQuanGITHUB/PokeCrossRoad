270a0998db9076689b58343a986fa8cc5cf8ca34
[HoangQuanGITHUB] <mcinotgoodatgame@gmail.com>
2023-10-22 15:32:35 +0700

    idk

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from ursina import *
-import panda3d.bullet as blt
 
-app = Ursina(borderless=False)
+app = Ursina(borderless = False)
+
+import panda3d.bullet as blt
 
 debugNode = blt.BulletDebugNode('Debug')
 debugNode.showWireframe(True)
@@ -16,34 +17,38 @@ world.setGravity(Vec3(0, -9.81, 0))
 world.setDebugNode(debugNP.node())
 
 # Ground
-ground = Entity(model='plane', texture='grass', scale=(200, 1, 200))
-shape = blt.BulletBoxShape(Vec3(100.0, 1.0, 100.0))
+ground = Entity(model = 'plane', texture = 'grass', scale = (200, 1, 200))
+
+shape = blt.BulletBoxShape(Vec3(100.0, 0.100, 100.0))
+
 collision_node = blt.BulletRigidBodyNode('Rigidbody')
 collision_node.addShape(shape)
+
 ground_np = render.attachNewNode(collision_node)
 ground_np.setPos(0, -2, 0)
 world.attachRigidBody(collision_node)
 ground.parent = ground_np
-ground.y = 1
 
 def spawn_cube():
-    # Cube
-    cube = Entity(model='cube', texture='brick')
-    shape2 = blt.BulletBoxShape(Vec3(.5, .5, .5))
-    node = blt.BulletRigidBodyNode('Rigidbody')
-    node.setMass(1)
-    node.addShape(shape2)
-    np = render.attachNewNode(node)
-    np.setPos(0, 50, 0)
-    world.attachRigidBody(node)
-    cube.parent = np
+    cube = Entity(model = 'cube', texture = 'brick')
+
+    shape = blt.BulletBoxShape(Vec3(.5, .5, .5))
+
+    collision_node = blt.BulletRigidBodyNode('Rigidbody')
+    collision_node.setMass(1)
+    collision_node.addShape(shape)
+
+    cube_np = render.attachNewNode(collision_node)
+    cube_np.setPos(0, 50, 0)
+    world.attachRigidBody(collision_node)
+    cube.parent = cube_np
+
+
+spawnButton_1 = Button(model = 'cube', color = color.green, parent = scene)
+spawnButton_1.on_click = spawn_cube
 
 def update():
     world.doPhysics(time.dt)
-
-def input(key):
-    if key == 'space up':
-        spawn_cube()
 
 Sky()
 
